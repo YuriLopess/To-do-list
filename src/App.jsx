537c6a929c47dae0aea3b925a780c137311b9e81
YuriLopess <costalopesyuri@gmail.com>
2024-07-08 14:46:57 -0300

    Adding delete and complete functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,19 @@ function App() {
     {
       id: 1,
       text: "Go to workout.",
-      category: "(Personal)",
+      category: "Personal",
       isCompleted: false,
     },
     {
       id: 2,
       text: "Sleep at 9 o'clock.",
-      category: "(Personal)",
+      category: "Personal",
       isCompleted: false,
     },
     {
       id: 3,
       text: "Make a snack after studying",
-      category: "(Personal)",
+      category: "Personal",
       isCompleted: false,
     }
   ])
@@ -35,12 +35,25 @@ function App() {
     setToDo(newToDo)
   }
 
+  const removeToDo = (id) => {
+    const newToDo = [...toDo]
+    const filterToDo = newToDo.filter(toDo => toDo.id !== id);
+    setToDo(filterToDo);
+  }
+
+  const completeToDo = (id) => {
+    const newToDo = toDo.map((toDo) =>
+      toDo.id === id ? { ...toDo, isCompleted: !toDo.isCompleted } : toDo
+    );
+    setToDo(newToDo);
+  }
+
   return (
     <div className='app'>
       <h1>Task List</h1>
       <div className='to-do-list'>
         {toDo.map((toDo) => (
-          <ToDo key={toDo.id} toDo={toDo}/>
+          <ToDo key={toDo.id} toDo={toDo} removeToDo={removeToDo} completeToDo={completeToDo}/>
         ))}
       </div>
       <Form addToDo={addToDo}/>
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
-const ToDo = ({toDo}) => {
+const ToDo = ({toDo, removeToDo, completeToDo}) => {
     return (
-        <div className="toDo">
+        <div className="toDo" style={{textDecoration: toDo.isCompleted ? "line-through" : ""}}>
             <div className="content">
                 <p>{toDo.text}</p>
                 <p className='category'>{toDo.category}</p>
             </div>
             <div>
-                <button className='complete'>Complete</button>
-                <button className='delete'>Delete</button>
+                <button className='complete' onClick={() => completeToDo(toDo.id)}>Complete</button>
+                <button className='delete' onClick={() => removeToDo(toDo.id)}>Delete</button>
             </div>
         </div>
     )
